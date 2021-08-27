1295f36d2603abdfff2e718f9388e13f827702a0
Philippe Trudel <p.trudel1996@gmail.com>
2021-08-27 00:42:20 -0400

    passing props to components

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <component v-bind:is="component" v-for="component in components" :key="component" />
+  <component v-bind:is="component.cpn" v-bind="component" v-for="component in components" :key="component.name" />
 </div>
 </template>
 
@@ -15,8 +15,9 @@ export default {
   },
 
   // You can call them by name or by components, even by renaming them
+  // you can even pass props to them with the v-bind
   data: () => ({
-    components : ["kek", 'kekw',]
+    components : [{cpn: 'kek', title: 'Counter'}, {cpn: 'kekw', name: 'Todo'},]
   })
 }
 </script>
